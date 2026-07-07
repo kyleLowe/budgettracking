35464dabe7cffdfd736ce8c8ae35de7f1e04b313
kyleLowe <kylelowe358@gmail.com>
2026-07-08 00:26:55 +1200

    Feat: Checking parameters input for CurrencyServices

--- backend/src/services/CurrencyServices.ts
+++ backend/src/services/CurrencyServices.ts
@@ -1,12 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import * as CurrencyDAO from '../daos/CurrencyDAO';
+import { NotAcceptableError } from '../errors/NotAcceptable';
 
 export const getCurrencyByISOCode = async (ISOCode: string) => {
-    const currency = CurrencyDAO.getCurrencyByISOCode(ISOCode);
+    if (ISOCode.length !== 3) {
+        throw new NotAcceptableError('Invalid ISO code. It must be a 3-letter code.');
+    }
+    const currency = CurrencyDAO.getCurrencyByISOCode(ISOCode.toUpperCase());
     return currency;
 };
 
 export const getCurrencyByCountry = async (country: string) => {
+    if (country.length < 2 || country.length > 56) {
+        throw new NotAcceptableError('Invalid country name. It must be between 2 and 56 characters.');
+    }
     const currency = CurrencyDAO.getCurrencyByCountry(country);
     return currency;
 }
